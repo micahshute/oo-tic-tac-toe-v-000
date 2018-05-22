1892ed7f73614f043298b8e86086e2829c96acd9
Micah Shute <micah.shute@gmail.com>
2018-05-22 16:15:57 +0000

    automatically backed up by learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,17 @@
+class TicTacToe
+
+
+  initialize
+    @board = Array.new(9, " ")
+  end
+
+  def move(index, player)
+    if valid_move?(index)
+      @board[index] = player
+      return true
+    end
+    return false
+  end
+
+  
+end
