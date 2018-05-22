18fcda9385519f3184b02b6df0ea4c35ba05cd0f
Micah Shute <micah.shute@gmail.com>
2018-05-22 16:33:34 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,6 +3,8 @@ class TicTacToe
 
   initialize
     @board = Array.new(9, " ")
+    @player_x = "X"
+    @player_o = "O"
   end
 
   def move(index, player)
@@ -11,6 +13,31 @@ class TicTacToe
       return true
     end
     return false
+  end
+
+  def input_to_index(input)
+    input - 1
+  end
+
+  def is_even(num)
+    num % 2 == 0
+  end
+  
+  def current_player
+    is_even(@board.reject{|space| space != "X" || space != "O"}.length) ? player_x : player_o
+  end
+
+  def turn
+    puts "Enter a number bewteen 1-9:"
+    index = input_to_index(gets.strip)
+    move(index, current_player)
+  end
+  
+  def play  
+    until over?
+      turn
+    end
+    puts won? ? "Congratulations, #{winner}" : "Cat's Game!"    
   end
 
 
